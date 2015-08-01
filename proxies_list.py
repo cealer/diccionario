a0da82f5798bfe02b4306adc150943b521a0f72b
cealer <cesar_gonzales_aguije@hotmail.com>
2015-07-31 23:51:18 -0500

    Scrapper de ip lista

--- proxies_list.py
+++ proxies_list.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+lista=[]
 driver = webdriver.Firefox()
 driver.get('http://www.cool-proxy.net/proxies/http_proxy_list/sort:score/direction:desc')
 html = driver.page_source
 soup = BeautifulSoup(html)
 for tag in soup.find_all('td'):
- 	print (tag.text)
+	lista.append(tag.text)
+#cadena = 'document.write(Base64.decode(str_rot13("ZwRmYwRmAv43BF4kZwD=")))213.136.79.124'
+#data = str(lista[0])
+#print(data.lstrip(")))" ))
+for x in range(len(lista)):
+	if not lista[x].find("document")==-1:
+		a=lista[x].find(')))')
+		a=a+3
+		print(lista[x][a:])
